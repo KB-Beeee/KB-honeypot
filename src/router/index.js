a84c7cdb9803f99b25a82773b3c9607284060dff
“im-minji” <mjlim514@gmail.com>
2026-04-08 11:03:41 +0900

    init: 초기 세팅

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 
 import Login from '../pages/Login.vue'
 import Mypage from '../pages/Mypage.vue'
-import AddTransaction from '../pages/AddTransaction.vue'
+// import AddTransaction from '../pages/AddTransaction.vue'
 import Home from '../pages/Home.vue'
 import TransactionList from '../pages/TransactionList.vue'
 
@@ -23,17 +23,17 @@ const routes = [
     name: 'list',
     component: TransactionList
   },
-  { 
-    path: '/add', 
-    name: 'add',
-    component: AddTransaction
-  },
-  { 
-    path: '/edit/:id', 
-    name: 'edit',
-    component: AddTransaction, // 등록 페이지 재사용
-    props: true // URL의 id를 컴포넌트의 props로 전달
-  },
+  // { 
+  //   path: '/add', 
+  //   name: 'add',
+  //   component: AddTransaction
+  // },
+  // { 
+  //   path: '/edit/:id', 
+  //   name: 'edit',
+  //   component: AddTransaction, // 등록 페이지 재사용
+  //   props: true // URL의 id를 컴포넌트의 props로 전달
+  // },
   { 
     path: '/profile', 
     name: 'profile',
