ebc2b768d12eb9ef1f8a1e755bcd08f004af6b8b
yumin0411 <62548846+yumin0411@users.noreply.github.com>
2026-04-09 09:33:30 +0900

    feat: 필터링 및 정렬 로직 구현 (#23)

---
+++ src/stores/transactionStore.js
@@ -0,0 +1,33 @@
+import { defineStore } from 'pinia';
+import { reactive, computed } from 'vue';
+import axios from 'axios';
+
+export const useTransactionStore = defineStore('transaction', () => {
+  const BASEURI = 'http://localhost:3000/transactions';
+
+  const state = reactive({
+    transactions: [],
+    categories: [],
+  });
+  const fetchTransactions = async () => {
+    try {
+      const response = await axios.get(BASEURI);
+      state.transactions = response.data;
+    } catch (error) {
+      console.error('데이터 로드 실패:', error);
+    }
+  };
+
+  const fetchCategories = async () => {
+    try {
+      const response = await axios.get('http://localhost:3000/categories');
+      state.categories = response.data;
+    } catch (error) {
+      console.error("카테고리 로드 실패:", error);
+    }
+  };
+
+  const transactions = computed(() => state.transactions);
+  const categories = computed(() => state.categories);
+  return { transactions, categories, fetchTransactions, fetchCategories };
+});
