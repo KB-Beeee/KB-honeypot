1965c856fa9ca37d06cc0519f2ef0381eca80cf3
“im-minji” <mjlim514@gmail.com>
2026-04-09 14:15:35 +0900

    [#32] fix: 경로 수정

--- src/components/Bar.vue
+++ src/components/Bar.vue
@@ -1,7 +1,8 @@
 <template>
-  <nav class="navbar shadow-sm">
+  <nav class="navbar shadow-sm" v-if="$route.path !== '/'">
     <div class="container-fluid d-flex align-items-center">
-      <router-link to="/" class="navbar-brand d-flex align-items-center">
+      
+      <router-link to="/home" class="navbar-brand d-flex align-items-center">
         <img
           src="@/assets/images/꿀벌/기본꿀벌.png"
           alt="Honeypot Logo"
@@ -11,7 +12,7 @@
       </router-link>
 
       <div class="nav-links d-flex">
-        <router-link to="/">홈</router-link>
+        <router-link to="/home">홈</router-link>
         <router-link to="/list">거래내역</router-link>
         <router-link to="/mypage">마이페이지</router-link>
       </div>
